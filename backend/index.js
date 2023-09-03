6488d1bdc806dd7f9cbd1f9ef814c75f24a61091
sudo-chmod-711 <chamodhhenderson@gmail.com>
2023-09-03 14:58:50 +0530

    Added MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 8080
+const DB_URL = process.env.DB_URL
 
 app.use(cors())
 app.use(express.json())
@@ -12,6 +14,12 @@ app.use((req, res, next) => {
     next()
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is running on PORT:${ PORT }`)
-})
+mongoose.connect(DB_URL)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on PORT:${ PORT }`)
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+    })

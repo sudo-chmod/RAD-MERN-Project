7a8ea622f04ef852b44d884f9d5759b9661637f7
sudo-chmod-711 <chamodhhenderson@gmail.com>
2023-09-09 15:02:05 +0530

    update hall routes

--- backend/routes/hallRoutes.js
+++ backend/routes/hallRoutes.js
@@ -4,11 +4,11 @@ const { addHall, viewAllHalls, viewHall, updateHall, removeHall } = require('../
 const { isAuth, isWho } = require('../controllers/authControllers')
 
 
-router.post('/add', isAuth, isWho('admin', 'teacher'), addHall)
+router.post('/add', isAuth, isWho('admin'), addHall)
 router.get('/', isAuth, viewAllHalls)
 router.get('/:id', isAuth, viewHall)
-router.delete('/edit/:id', isAuth, isWho('admin', 'teacher'), removeHall)
-router.put('/edit/:id', isAuth, isWho('admin', 'teacher'), updateHall)
+router.delete('/edit/:id', isAuth, isWho('admin'), removeHall)
+router.put('/edit/:id', isAuth, isWho('admin'), updateHall)
 
 
 module.exports = router
